e79f82799b4c7f9ca4caa686b75cfef3cc861a0d
dottor5 <dluccash@hotmail.com>
2020-06-12 19:59:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# semana-ominstack-7
+ 
---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+const cors = require("cors")
+const routes = require("./routes")
+
+const app = express()
+
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+mongoose.connect(
+  "mongodb+srv://admin:Pdz294UPQsrxuR1s@cluster0-m1yml.gcp.mongodb.net/week7?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+app.use((req, res, nexr) => {
+  req.io = io
+  next()
+})
+app.use(cors())
+
+app.use(routes)
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+)
+
+app.listen(3333)
